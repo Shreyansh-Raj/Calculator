373c49d06fe033ee370cccb5be409d1478a421be
Shreyansh Raj <67150331+Shreyansh-Raj@users.noreply.github.com>
2021-12-01 19:46:54 +0530

    Removed some devlopment code

--- src/index.js
+++ src/index.js
@@ -18,10 +18,6 @@ const createWindow = () => {
 		}
 	});
 
-	// Create reload shortcut
-	globalShortcut.register('f5', function () {
-		mainWindow.reload()
-	})
 
 	// Change Title
 	mainWindow.setTitle("Calculator")
@@ -35,8 +31,6 @@ const createWindow = () => {
 	// and load the index.html of the app.
 	mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-	// Open the DevTools.
-	mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
